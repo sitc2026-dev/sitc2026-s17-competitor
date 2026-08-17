1af915589eaf547a22673ce420479d78dae5c430
Zoltan Sisak <zoltan.sisak@gmail.com>
2026-08-17 21:51:57 +0200

    add module a assets

---
+++ assets/module-a/B1/buildings.js
@@ -0,0 +1,27 @@
+const buildings = [
+  {
+    name: "Merdeka 118",
+    height: 678.9,
+    image: "/towers/merdeka-118.png",
+  },
+  {
+    name: "Taipei 101",
+    height: 508,
+    image: "/towers/taipei-101.png",
+  },
+  {
+    name: "Burj Khalifa",
+    height: 828,
+    image: "/towers/burj-khalifa.png",
+  },
+  {
+    name: "Oriental Pearl Tower",
+    height: 468,
+    image: "/towers/oriental-pearl-tower.png",
+  },
+  {
+    name: "Canton Tower",
+    height: 604,
+    image: "/towers/canton-tower.png",
+  },
+];
